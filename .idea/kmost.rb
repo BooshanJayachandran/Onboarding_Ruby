da764e8d216dfceceb6d7b52b1f24a8ac44ad06c
booshanjayachandran <booshan.jayachandran@go-yubi.com>
2024-04-02 16:24:44 +0530

    1 and 2

--- .idea/kmost.rb
+++ .idea/kmost.rb
@@ -1,2 +1,21 @@
 # frozen_string_literal: true
+def top_k_frequent(arr, k)
+  a = arr.each_with_object(Hash.new(0)) { |n,h| h[n] += 1 }
+  b = a.sort_by { |_, count| -count }
+  c = b.first(k).map(&:first)
+end
 
+arr = []
+puts "Enter the number of elements in the array:"
+n = gets.chomp.to_i
+
+puts "Enter the elements of the array:"
+for i in (0...n)
+  arr[i] = gets.chomp.to_i
+end
+
+puts "Enter the value of k:"
+k = gets.chomp.to_i
+
+c = top_k_frequent(arr, k)
+puts "Top #{k} most frequent : #{c.join(', ')}"
--- meeting_room.rb
+++ meeting_room.rb
@@ -1,4 +1,33 @@
 # frozen_string_literal: true
+def canAttend(two_d_array)
+  if two_d_array.nil? || two_d_array.length==1
+    true
+  end
+  sorted_two_d_array = two_d_array.sort_by { |sub_array| sub_array[0] }
+  puts "array after sorting #{sorted_two_d_array.inspect}"
+  (0...sorted_two_d_array.length - 1).each do |i|
+    if sorted_two_d_array[i][1] > sorted_two_d_array[i + 1][0]
+      return false
+    end
+  end
 
-class MeetingRoom
+  return true
+end
+puts "no of tries"
+z=gets.chomp.to_i
+(0...z).each do |i|
+ puts "enter the row size of array"
+ a=gets.chomp.to_i
+ puts "enter the column size of array"
+ b=gets.chomp.to_i
+ two_d_array = Array.new(a) { Array.new(b) }
+ puts "enter the elements"
+ (0...two_d_array.length).each do |i|
+   (0...(two_d_array[i].length)).each do |j|
+     two_d_array[i][j]=gets.chomp
+   end
+ end
+ puts "array is #{two_d_array.inspect}"
+ result=canAttend(two_d_array)
+ puts "#{result}"
 end
